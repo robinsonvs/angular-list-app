afd9068ed9a47c0562bb5b2a37c54b2cc5085314
Robinson Severo <robinsonvs@gmail.com>
2018-11-17 21:26:56 -0200

    adding shared module and interfaces

--- angular-list-app/src/app/app.module.ts
+++ angular-list-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CustomersModule } from './customers/customers.module';
+import { SharedModulo } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule ],
+  imports:      [ BrowserModule, CustomersModule, SharedModulo ],
   declarations: [ AppComponent ],
   providers:    [],
   bootstrap:    [AppComponent]
--- angular-list-app/src/app/customers/customers-list/customers-list.component.ts
+++ angular-list-app/src/app/customers/customers-list/customers-list.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 
+import { ICustomer } from '../../shared/interfaces';
+
 @Component({
     selector: 'app-customers-list',
     templateUrl: './customers-list.component.html'
@@ -8,10 +10,10 @@ import { Component, OnInit } from '@angular/core';
 
 export class CustomersListComponent implements OnInit {
 
-    filteredCustomers: any[] = [];
+    filteredCustomers: ICustomer[] = [];
     customersOrderTotal: number;
     currencyCode: string = 'USD';
-    
+
     constructor() {}
 
     ngOnInit() {
