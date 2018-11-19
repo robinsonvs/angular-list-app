e5b15cdee23bdfbc5aecdfef3007d5c51be418ff
Robinson Severo <robinsonvs@gmail.com>
2018-11-19 00:54:19 -0200

    data binding - working with pipes

--- angular-list-app/src/app/customers/customers.module.ts
+++ angular-list-app/src/app/customers/customers.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
+import { SharedModulo } from '../shared/shared.module';
 
 @NgModule({
-  imports:      [ CommonModule ],
+  imports:      [ CommonModule, SharedModulo ],
   declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   exports: [ CustomersComponent ]
 })
