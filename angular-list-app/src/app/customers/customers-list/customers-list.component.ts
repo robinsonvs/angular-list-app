1861a7962b9dc13c29554aee0a2235854f08e3a9
Robinson Severo <robinsonvs@gmail.com>
2018-11-18 23:57:14 -0200

    data binding - event binding

--- angular-list-app/src/app/customers/customers-list/customers-list.component.ts
+++ angular-list-app/src/app/customers/customers-list/customers-list.component.ts
@@ -17,12 +17,6 @@ export class CustomersListComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {
-        this.filteredCustomers = [
-            { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
-            { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
-            { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
-            { id: 4, name: 'Jim Thomas', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
-        ];
     }
 
     calculateOrders() {
@@ -30,5 +24,9 @@ export class CustomersListComponent implements OnInit {
         this.filteredCustomers.forEach((cust: ICustomer) => {
             this.customersOrderTotal += cust.orderTotal;
         })
+    }
+
+    sort(prop: string) {
+
     }
 }
