7a8383d8e02f23abb43cf040fbeaa7dbb9865062
Robinson Severo <robinsonvs@gmail.com>
2018-12-10 22:39:19 -0200

    services and http - injecting a service into a component

--- angular-list-app/src/app/app.module.ts
+++ angular-list-app/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModulo } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule, SharedModulo ],
+  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModulo ],
   declarations: [ AppComponent ],
   providers:    [],
   bootstrap:    [AppComponent]
