021a8219a42273f4521f57239977312285e9fef2
Robinson Severo <robinsonvs@gmail.com>
2018-12-11 23:52:06 -0200

    routing - creating a routing module with routes

--- angular-list-app/src/app/app.module.ts
+++ angular-list-app/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModulo } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
-  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModulo ],
+  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModulo, AppRoutingModule ],
   declarations: [ AppComponent ],
   providers:    [],
   bootstrap:    [AppComponent]
