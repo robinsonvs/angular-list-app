071f25e0ba333b1e097a8d793cff8c3e642a093c
Robinson Severo <robinsonvs@gmail.com>
2018-12-12 22:23:57 -0200

    routing - adding an orders component with routes

---
+++ angular-list-app/src/app/orders/orders.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { OrdersComponent } from './orders.component';
+import { OrderRoutingModule } from './orders-routing.module';
+
+@NgModule({
+    imports: [ CommonModule, FormsModule, SharedModule, OrderRoutingModule ],
+    declarations: [ OrdersComponent ]
+})
+
+export class OrdersModule { }
